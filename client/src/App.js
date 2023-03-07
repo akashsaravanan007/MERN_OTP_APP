2b76a83df143bfe764844235ca522ddfb1a70470
akashsaravanan007 <58935531+akashsaravanan007@users.noreply.github.com>
2023-03-07 10:40:13 +0530

    client updated
    client third commit

---
+++ client/src/App.js
@@ -0,0 +1,59 @@
+import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import Username from "./components/Username";
+import Password from "./components/Password";
+import Register from "./components/Register";
+import Profile from "./components/Profile";
+import Recovery from "./components/Recovery";
+import Reset from "./components/Reset";
+import PageNotFound from "./components/PageNotFound";
+
+import { AuthorizeUser, ProtectRoute } from "./middleware/auth";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Username></Username>,
+  },
+  {
+    path: "/register",
+    element: <Register></Register>,
+  },
+  {
+    path: "/password",
+    element: (
+      <ProtectRoute>
+        <Password />
+      </ProtectRoute>
+    ),
+  },
+  {
+    path: "/profile",
+    element: (
+      <AuthorizeUser>
+        <Profile />
+      </AuthorizeUser>
+    ),
+  },
+  {
+    path: "/recovery",
+    element: <Recovery></Recovery>,
+  },
+  {
+    path: "/reset",
+    element: <Reset></Reset>,
+  },
+  {
+    path: "*",
+    element: <PageNotFound></PageNotFound>,
+  },
+]);
+
+export default function App() {
+  return (
+    <main>
+      <RouterProvider router={router}></RouterProvider>
+    </main>
+  );
+}
